9e71c3105b2c42dd8c6c810d62e29e98aea0ba9c
boichique <iuvarov97@gmail.com>
2023-08-31 00:00:49 +0300

    swagger fmt

--- internal/modules/users/handler.go
+++ internal/modules/users/handler.go
@@ -13,15 +13,15 @@ func NewHandler(service *Service) *Handler {
 	return &Handler{service: service}
 }
 
-// @Summary		Create a new user
-// @Description	Create a new user with the given user ID
-// @Accept			json
-// @Produce		json
-// @Param			userID	path		int		true	"User ID"
-// @Success		200		{string}	string	"OK"
-// @Failure		400		{string}	string	"Bad Request"
-// @Failure		500		{string}	string	"Internal Server Error"
-// @Router			/api/users/{userID} [post]
+//	@Summary		Create a new user
+//	@Description	Create a new user with the given user ID
+//	@Accept			json
+//	@Produce		json
+//	@Param			userID	path		int		true	"User ID"
+//	@Success		200		{string}	string	"OK"
+//	@Failure		400		{string}	string	"Bad Request"
+//	@Failure		500		{string}	string	"Internal Server Error"
+//	@Router			/api/users/{userID} [post]
 func (h *Handler) Create(c echo.Context) error {
 	req, err := echox.Bind[CreateUserRequest](c)
 	if err != nil {
@@ -35,15 +35,15 @@ func (h *Handler) Create(c echo.Context) error {
 	return h.service.Create(c.Request().Context(), user)
 }
 
-// @Summary		Delete a user
-// @Description	Delete a user by user ID
-// @Accept			json
-// @Produce		json
-// @Param			userID	path		int		true	"User ID"
-// @Success		200		{string}	string	"OK"
-// @Failure		400		{string}	string	"Bad Request"
-// @Failure		500		{string}	string	"Internal Server Error"
-// @Router			/api/users/{userID} [delete]
+//	@Summary		Delete a user
+//	@Description	Delete a user by user ID
+//	@Accept			json
+//	@Produce		json
+//	@Param			userID	path		int		true	"User ID"
+//	@Success		200		{string}	string	"OK"
+//	@Failure		400		{string}	string	"Bad Request"
+//	@Failure		500		{string}	string	"Internal Server Error"
+//	@Router			/api/users/{userID} [delete]
 func (h *Handler) Delete(c echo.Context) error {
 	req, err := echox.Bind[DeleteUserRequest](c)
 	if err != nil {
